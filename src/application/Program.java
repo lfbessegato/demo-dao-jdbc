8c1a0a775a58a6f39ed1644e9b262846e05a6cde
Luciano Bessegato <lfrbessegato@gmail.com>
2019-09-20 13:56:21 -0300

    Update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,9 @@ import model.entities.Seller;
 
 public class Program {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		
@@ -35,11 +38,17 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 4: seller inser ===");
+		System.out.println("\n=== TEST 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
-
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
 	
 	}
 
